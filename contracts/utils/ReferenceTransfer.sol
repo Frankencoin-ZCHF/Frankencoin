2ccca63c6227441c7927afb58581b7a618772cab
samclassix <samclassix@proton.me>
2025-05-08 08:06:31 +0200

    feat: added target to auto save

--- contracts/utils/ReferenceTransfer.sol
+++ contracts/utils/ReferenceTransfer.sol
@@ -8,45 +8,48 @@ import "../interface/IERC20.sol";
  */
 contract ReferenceTransfer {
     IERC20 public immutable ZCHF;
-    ISavings public immutable SAVINGS;
-
     uint256 internal constant INFINITY = (1 << 255); // @dev: copied from "./ERC20.sol"
 
-    mapping(address => bool) public hasAutoSave;
+    mapping(address => address) public hasAutoSave;
 
+    event AutoSave(address indexed to, address target);
     event Transfer(address indexed from, address indexed to, uint256 amount, string ref);
 
     error InfiniteAllowanceRequired(address owner, address spender);
 
-    constructor(address token, address savings) {
+    constructor(address token) {
         ZCHF = IERC20(token);
-        SAVINGS = ISavings(savings);
     }
 
     function transfer(address recipient, uint256 amount, string calldata ref) public returns (bool) {
+        _executeTransfer(msg.sender, recipient, amount);
         emit Transfer(msg.sender, recipient, amount, ref);
-        executeTransfer(msg.sender, recipient, amount);
         return true;
     }
 
     function transferFrom(address owner, address recipient, uint256 amount, string calldata ref) public returns (bool) {
         if (ZCHF.allowance(owner, msg.sender) < INFINITY) revert InfiniteAllowanceRequired(owner, msg.sender);
+        _executeTransfer(owner, recipient, amount);
         emit Transfer(owner, recipient, amount, ref);
-        executeTransfer(owner, recipient, amount);
         return true;
     }
 
-    function setAutoSave(bool enabled) external {
-        hasAutoSave[msg.sender] = enabled;
-    }
-
-    function executeTransfer(address from, address to, uint256 amount) internal {
-        if (hasAutoSave[to]) {
+    function _executeTransfer(address from, address to, uint256 amount) internal {
+        if (hasAutoSave[to] != address(0)) {
             ZCHF.transferFrom(from, address(this), amount);
-            SAVINGS.save(to, uint192(amount));
+            ISavings(hasAutoSave[to]).save(to, uint192(amount));
         } else {
             ZCHF.transferFrom(from, to, amount);
         }
+    }
+
+    function setAutoSave(address target) public {
+        hasAutoSave[msg.sender] = target;
+        emit AutoSave(msg.sender, target);
+    }
+
+    function clearAutoSave() external {
+        setAutoSave(address(0));
     }
 }
 
